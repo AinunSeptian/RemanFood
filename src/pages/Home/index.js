9f679654a543b0d41f704060ac4f307477122b6e
ainunseptian <ainunseptian@kali.ainunseptian>
2022-01-31 02:49:16 -0500

    create header user and food card component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,50 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {
+  FoodDummy1,
+  FoodDummy2,
+  FoodDummy3,
+  FoodDummy4,
+  ProfileDummy,
+} from '../../assets';
+import {FoodCard, Gap} from '../../components';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Page</Text>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>RemanFood</Text>
+          <Text style={styles.desc}>Let's get some foods</Text>
+        </View>
+        <Image source={ProfileDummy} style={styles.profile} />
+      </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={styles.foodCardContainer}>
+          <Gap width={24} />
+          <FoodCard image={FoodDummy1} />
+          <FoodCard image={FoodDummy3} />
+          <FoodCard image={FoodDummy2} />
+          <FoodCard image={FoodDummy4} />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingTop: 32,
+    paddingBottom: 24,
+    backgroundColor: 'white',
+  },
+  appName: {fontSize: 22, fontFamily: 'Poppins-Medium', color: '#020202'},
+  desc: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8d92a3'},
+  profile: {width: 50, height: 50, borderRadius: 8},
+  foodCardContainer: {flexDirection: 'row', marginVertical: 24},
+});
